6cc83984bc9f1a00ae02e2f891cbb3476bb7921d
Fredy Kcrez <zholary@gmail.com>
2018-06-18 19:20:00 -0600

    Programación 3: Ejercicios Guía 7 - 2012 Ejemplos de la guía 7

---
+++ G7_ClasesAsociadasCompuestas/src/empleado/Empleado.java
@@ -0,0 +1,23 @@
+package empleado;
+
+/**
+ * @date 18/06/2018
+ * @author Fredy Kcrez
+ */
+public class Empleado {
+    private String primerNombre, apellidoPaterno;
+    private Fecha fechaNacimiento, fechaContratacion;
+
+    //Constructor para inicializar nombre, fecha de nacimiento y fecha de contratación
+    Empleado(String nombre, String apellido, Fecha fechaDeNacimiento, Fecha fechaDeContratacion) {
+        this.primerNombre = nombre;
+        this.apellidoPaterno = apellido;
+        this.fechaNacimiento = fechaDeNacimiento;
+        this.fechaContratacion = fechaDeContratacion;
+    }
+    
+    //Convierte Empleado a format String
+    public String toString() {
+        return String.format("%s, %s  Contratado: %s  Cumpleanios: %s", apellidoPaterno, primerNombre, fechaContratacion, fechaNacimiento);
+    }
+}
